f4762125c92e313c39f436c4d63c06b993854b12
meshbound <60497577+meshbound@users.noreply.github.com>
2020-02-29 12:50:37 -0600

    Added templates
    Added the base program for the turret and the base file for producing test data.

---
+++ GroundControl/serialRecievePseudo.py
@@ -0,0 +1,104 @@
+import serial
+import csv
+import time
+import os
+import json
+import datetime
+
+SERIAL_PORT = "COM4"
+BAUD_RATE = 9600
+
+# ser = se rial.Serial(SERIAL_PORT, BAUD_RATE)
+date = datetime.date.today()
+
+path = "./Data/"
+
+dump_path_temp = "{0}dump-({1}).csv"
+dump_path_finial = ""
+temp_path = "{}Current.json".format(path)
+
+start_dump = path + str(date) + "-"
+
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+
+ser.write(str.encode("-"))  # "-" skips the fix for the GPS
+time.sleep(1)
+ser.write(str.encode("+"))  # telemetry module will not transmit data until "+" is sent
+
+data_raw_dict = {"!!": "", "@@": "", "##": "", "%%": "", "$$": ""}
+
+data_clean_dict = {"bmp": {"temperature": ""},
+
+                   "ccs": {"co2": "", "tvoc": ""},
+
+                   "baro": {"pressure": "", "altitude": ""},
+
+                   "bno": {"quaternion ": {"quat_w": "", "quat_x": "", "quat_y": "", "quat_z": ""},
+                           "mag": {"mag_x": "", "mag_y": "", "mag_z": ""},
+                           "gyroscope": {"gyro_x": "", "gyro_y": "", "gyro_z": ""},
+                           "accelerometer": {"accel_x": "", "accel_y": "", "accel_z": ""}
+                           },
+
+                   "gps": {"time": {"hour": "", "min": "", "sec": "", "milli": "", "day": "", "month": "", "year": ""},
+                           "connection": {"fix": "", "fix_quality": ""},
+                           "positon": {"latitude": "", "lat": "", "longitude": "", "long": ""},
+                           "info": {"speed": "", "angle": "", "altitude": "", "satellites": ""}
+                           }
+                   }
+
+
+def apply_to_dict(key, data, offset):
+    i = 0 + offset
+    for sub_key in data_clean_dict[key].items():
+        for sub_sub_key in data_clean_dict[key][sub_key[0]]:
+            data_clean_dict[key][sub_key[0]][sub_sub_key] = data[i]
+            i += 1
+
+def proper_format(tag, incoming):
+    data = incoming.replace(tag + ',', "").split(',')
+    offset = 1
+
+    if (tag == "!!"):
+        data_clean_dict["bmp"]["temperature"] = data[offset + 1];
+    elif (tag == "@@"):
+        data_clean_dict["ccs"]["co2"] = data[offset + 1];
+        data_clean_dict["ccs"]["tvoc"] = data[offset + 2];
+    elif (tag == "##"):
+        data_clean_dict["baro"]["pressure"] = data[offset + 1];
+        data_clean_dict["baro"]["altitude"] = data[offset + 2];
+    elif (tag == "%%"):
+        apply_to_dict("bno", data, offset+1)
+    elif (tag == "$$"):
+        apply_to_dict("gps", data, offset+1)
+
+    return data_clean_dict
+
+
+current = 0
+while True:
+    testName = dump_path_temp.format(start_dump, current)
+    if os.path.exists(testName):
+        current += 1
+    else:
+        dump_path_finial = testName
+        break;
+
+while True:
+    incoming_raw = ser.readline().strip().decode("utf-8")
+    incoming_split = incoming_raw.split(",")
+
+    tag = incoming_split[0]
+
+    print(incoming_split)
+
+    with open(dump_path_finial, 'a') as dataDump:
+        writer = csv.writer(dataDump)
+        writer.writerow(incoming_split)
+        dataDump.close()
+
+    with open(temp_path, 'w') as dataTemp:
+        if tag in data_raw_dict:
+            json.dump(proper_format(tag, incoming_raw), dataTemp, default=json)
+        dataTemp.close()
+
+ser.close()
